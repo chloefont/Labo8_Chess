b4efdc9ffc2d140507de088ca1ecf118fe033d6c
Luca Coduri <coduri.vitor@gmail.com>
2022-01-02 21:22:42 +0100

    voila

--- src/engine/rules/Movements/MoveLinear.java
+++ src/engine/rules/Movements/MoveLinear.java
@@ -6,8 +6,21 @@ import game.GameBoard;
 import game.Vector;
 
 public class MoveLinear extends Movement {
+    private final Vector direction;
+
+    // pour ne pas tout casser;
+    MoveLinear(){
+        direction = new Vector(0,0);
+    }
+
+    MoveLinear(Vector direction){
+        this.direction = direction;
+    }
+
+
     @Override
     public boolean check(GameBoard board, Piece piece, Vector to) {
-        return super.checkLines(board, piece, to) && super.checkNoPieceBetween(board, piece, to);
+        // On check que le destination se trouve bien dans la bonne direction et qu'aucun pion ne se trouve entre la piece et la destination.
+        return direction.colinear(to.sub(piece.getPosition())) && super.checkNoPieceBetween(board, piece, to);
     }
 }

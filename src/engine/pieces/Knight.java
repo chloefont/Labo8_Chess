6c452cefa84177a78a67c6dbb0e417d1f5d1ce52
chloefont <cfontaine@citycable.ch>
2022-01-02 18:27:12 +0100

    déplacement en L

--- src/engine/pieces/Knight.java
+++ src/engine/pieces/Knight.java
@@ -2,12 +2,19 @@ package engine.pieces;
 
 import chess.PieceType;
 import chess.PlayerColor;
+import engine.rules.Movements.MoveForward;
+import engine.rules.Movements.MoveL;
+import engine.rules.Movements.MoveLinear;
+import engine.rules.Movements.Movement;
 import game.GameBoard;
 import game.Vector;
 
 public class Knight extends Piece {
     public Knight(GameBoard gameBoard, PlayerColor color, Vector position) {
         super(gameBoard, color, position);
+
+        Movement[] movementRules = {new MoveL()};
+        setMovementRules(movementRules);
     }
 
     @Override
--- src/engine/rules/Movements/MoveL.java
+++ src/engine/rules/Movements/MoveL.java
@@ -7,6 +7,9 @@ import game.Vector;
 public class MoveL extends Movement {
     @Override
     public boolean check(GameBoard board, Piece piece, Vector to) {
-        return false;
+        Vector diff = piece.getPosition().sub(to);
+
+        return ((diff.getX() == 2 || diff.getX() == -2) && (diff.getY() == 1 || diff.getY() == -1))
+                || ((diff.getY() == 2 || diff.getY() == -2) && (diff.getX() == 1 || diff.getX() == -1));
     }
 }
